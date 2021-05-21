8c41acc0972924a7e450888158d4e702f52ce42f
Sander-riis <74243825+Sander-riis@users.noreply.github.com>
2021-05-21 21:51:39 +0200

    Update HeroController.cs

--- DotaGrid.API/Controllers/HeroController.cs
+++ DotaGrid.API/Controllers/HeroController.cs
@@ -1,11 +1,125 @@
-﻿using System;
+﻿using DotaGrid.DataAccess;
+using DotaGrid.model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DotaGrid.API.Controllers
 {
-    public class HeroController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : ControllerBase
     {
+       
+    
+        private readonly HeroContext _context;
+
+        public HeroController(HeroContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Heroes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Hero>>> GetHeroes()
+        {
+            var heroes = _context.Heroes.Include(x => x.Mainattribute);
+
+            return await heroes.ToListAsync();
+        }
+
+        // GET: api/Heroes/5
+        [HttpGet("{id}")]
+        public ActionResult<Hero> GetHero(int id)
+        {
+            var hero = _context.Heroes.Include(x => x.Name).SingleOrDefault(r => r.HeroId == id);
+
+            if (hero == null)
+            {
+                return NotFound();
+            }
+
+            return hero;
+        }
+        /*
+        // GET: api/Heroes/5/Classes
+        [HttpGet("{id}/classes")]
+        public async Task<ActionResult<IEnumerable<Hero>>> getHeroesInMainattributes([FromRoute] int id)
+        {
+            return await (from am in _context.MainAttributes
+                            join m in _context.Classes on am.ClassId equals m.ClassId
+                            where am.RaceId == id
+                            select m).ToListAsync();
+        }
+        */
+
+        // PUT: api/Heroes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHero(int id, Hero hero)
+        {
+            if (id != hero.HeroId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(hero).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HeroExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Heroes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Hero>> PostHero(Hero hero)
+        {
+            _context.Heroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHero", new { id = hero.HeroId }, hero);
+        }
+
+        // DELETE: api/Heroes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Hero>> DeleteHero(int id)
+        {
+            var hero = await _context.Heroes.FindAsync(id);
+            if (hero == null)
+            {
+                return NotFound();
+            }
+
+            _context.Heroes.Remove(hero);
+            await _context.SaveChangesAsync();
+
+            return hero;
+        }
+
+        private bool HeroExists(int id)
+        {
+            return _context.Heroes.Any(e => e.HeroId == id);
+            
+        }
     }
 }
